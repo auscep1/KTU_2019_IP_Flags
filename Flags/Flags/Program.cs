58bb30dbc130fd964204ac574319df28db6fdf58
ausra <ausra.cepulionyte@ktu.edu>
2019-05-17 00:35:14 +0300

    Duomenų paruošimas ir valymas & Dimensijų sumažinimas

--- Flags/Flags/Program.cs
+++ Flags/Flags/Program.cs
@@ -8,7 +8,7 @@
  * Martynas Švykas, IFF-6/1 gr.
  * Jonas Jermakovičius, IFF-6/1 gr.
  * 
- * NOTES.txt !!!
+ * NOTES.txt
 */
 
 using System;
@@ -18,7 +18,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra; /*zr. NOTES.txt*/
+using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace Flags
@@ -39,15 +39,78 @@ namespace Flags
 		static void Main(string[] args)
 		{
 			string flagData = GetDataFromWeb();
-			Matrix<double> dataMatrix = ConvertToDataMatrix(flagData); 
-			Matrix<double> dataMatrixWithoutSunstar = dataMatrix.RemoveColumn(22); /*without sunstar attribute*/
+			Matrix<double> dataMatrix = ConvertToDataMatrix(flagData);
+			Matrix<double> dataMatrixNormalized = DataNormalization(dataMatrix).RemoveColumn(0); /*and remove country column*/
+			Matrix<double> covarianceMatrix = CovarianceMatrix(dataMatrixNormalized);
 
-			Matrix<double> covarianceMatrix = CovarianceMatrix(dataMatrixWithoutSunstar);
+			Dictionary<double, double> covarianceSunStarSorted = SortedSunsetCovariation(covarianceMatrix);
+			Dictionary<double, double> sortedByMostReflectAttributes = SortedByMostReflectAttributes(covarianceSunStarSorted);
 
-			Console.WriteLine(dataMatrix.ToString());
-			Console.WriteLine(covarianceMatrix.ToString());
+			Console.WriteLine("Data matrix: \n\r" + dataMatrix.ToString());
+			Console.WriteLine("Data matrix normalized: \n\r" + dataMatrixNormalized.ToString());
+			Console.WriteLine("Covariance matrix: \n\r" + covarianceMatrix.ToString());
+			Console.WriteLine("Covariance of Sunstar sorted attribute: \n\r" + String.Join("\n\r ", covarianceSunStarSorted));
+			Console.WriteLine("Covariance of Sunstar sorted by most reflected attributes: \n\r" + String.Join("\n\r ", sortedByMostReflectAttributes));
 
+			/*sortedByMostReflectAttributes key should be used for attributes selecion*/
+			/*experiments of clasificator starts:*/
+			int dimensionsQuantity = dataMatrixNormalized.ColumnCount-1; /* experiments for clasificator*/
+			while(dimensionsQuantity>1)
+			{
+				Matrix<double> dataMatrixReduced = GetReducedMatrix(dataMatrixNormalized, dimensionsQuantity, sortedByMostReflectAttributes);
+				
+				/*clasifikatorius, experimentai, kryzmine patikra...*/
+
+				dimensionsQuantity--;
+				Console.WriteLine("Reduced Data matrix: \n\r" +  dataMatrixReduced.ToString());
+			}
 			Console.ReadKey();
+		}
+
+		/*Get dimensions reduced matrix*/
+		static Matrix<double> GetReducedMatrix(Matrix<double> dataMatrixNormalized, int dimensionsQuantity, Dictionary<double, double> sortedByMostReflectAttributes)
+		{
+			Matrix<double> dataMatrixReduced = Matrix<double>.Build.Dense(dataMatrixNormalized.RowCount, dimensionsQuantity+1);
+			int counter = 1;
+			dataMatrixReduced.SetColumn(0, dataMatrixNormalized.Column(22));
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("Attributes in use: ");
+			foreach (KeyValuePair<double, double> kvp in sortedByMostReflectAttributes)
+			{
+				Console.Write("\t" + kvp.Key);
+				dataMatrixReduced.SetColumn(counter, dataMatrixNormalized.Column((int)kvp.Key));
+				counter++;
+				if (counter >= dimensionsQuantity+1)
+					break;
+			}
+			Console.Write("\n\r");
+			Console.ForegroundColor = ConsoleColor.White;
+			return dataMatrixReduced;
+		}
+
+		/*Get soreted by most reflect to sunset attributes: Return dictionary <real index of attribute , sorted probabilities>*/
+		static Dictionary<double, double> SortedByMostReflectAttributes(Dictionary<double, double> covarianceSunStarSorted)
+		{
+			Dictionary<double, double> covarianceSunStarSortedAbs = new Dictionary<double, double>(); //nuokrypis
+			foreach (KeyValuePair<double, double> kvp in covarianceSunStarSorted)
+			{
+				covarianceSunStarSortedAbs.Add(kvp.Key, Math.Abs(covarianceSunStarSorted[kvp.Key]));
+			}
+			covarianceSunStarSortedAbs= covarianceSunStarSortedAbs.OrderByDescending(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+			//var sortedDict = (from x in covarianceSunStarSortedAbs orderby x.Value descending select x).ToDictionary(pair => pair.Key, pair => pair.Value);
+			return covarianceSunStarSorted;
+		}
+
+		/*Get soreted by most reflect to sunset attributes: Return dictionary <real index of attribute , sorted probabilities>*/
+		static Dictionary<double, double> SortedSunsetCovariation(Matrix<double> covarianceMatrix)
+		{
+			Dictionary<double, double> covarianceSunStarSorted = new Dictionary<double, double>();
+			for (int i = 0; i < covarianceMatrix.RowCount; i++)
+			{
+				covarianceSunStarSorted.Add(i, covarianceMatrix[i, 22]);
+			}
+			covarianceSunStarSorted = covarianceSunStarSorted.OrderByDescending(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+			return covarianceSunStarSorted;
 		}
 
 		/* Calculate covariance*/
@@ -61,7 +124,8 @@ namespace Flags
 					double cov = Covariance(dataMatrix.Column(i), dataMatrix.Column(ii));
 					covarianceMatrix[i, ii] = cov;
 					covarianceMatrix[ii, i] = cov;
-					covarianceMatrix[i, i] = Dispersion(dataMatrix.Column(i));
+					//covarianceMatrix[i, i] = Dispersion(dataMatrix.Column(i));
+					covarianceMatrix[i, i] = 0;
 				}
 			}
 			return covarianceMatrix;
@@ -91,38 +155,67 @@ namespace Flags
 			double mean = arr.Average();
 			double sum = 0;
 			for (int i = 0; i < arrLength; i++)
-				sum = sum + Math.Pow((arr[i] - mean),2);
+				sum = sum + Math.Pow((arr[i] - mean), 2);
 			return sum / (arrLength - 1);
 		}
 
-	/* 
-	 * Get data matrix:
-	 * replace country name to index;
-	 * replace color to color number
-	*/
-	private static Matrix<double> ConvertToDataMatrix(string flagData)
-	{
-		int countryCounter = 0;
-		List<string> arrLines = flagData.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
-		Matrix<double> dataMatrix = Matrix<double>.Build.Dense(arrLines.Count, 30); /*matrix 194x30*/
-		foreach (string line in arrLines)
+		/*Data min-max normalization to [0,1]: v'=(v-minA)/(maxA-minA)*/
+		private static Matrix<double> DataNormalization(Matrix<double> dataMatrix)
 		{
-			string newLine = Regex.Replace(line, string.Join("|", ColorReplacements.Keys.Select(k => k.ToString()).ToArray()), m => ColorReplacements[m.Value]); /*replace color to number*/
-			string[] arr = newLine.Split(',').ToArray();
-			Country.Add(arr[0], countryCounter); /*add country to dictionary: key=country, value=number*/
-			arr[0] = countryCounter.ToString(); /*replace country to number*/
-			dataMatrix.SetRow(countryCounter, Array.ConvertAll(arr, double.Parse)); /*parse from string to to int array and add to matrix row*/
-			countryCounter++;
+			Matrix<double> dataMatrixNormalized = Matrix<double>.Build.Dense(dataMatrix.RowCount, dataMatrix.ColumnCount); /*matrix 194x30*/
+			for (int i = 0; i < dataMatrix.ColumnCount; i++)
+			{
+				dataMatrixNormalized.SetColumn(i, Normalization(dataMatrix.Column(i)));
+			}
+			return dataMatrixNormalized;
 		}
-		return dataMatrix;
-	}
 
-	/*Get flag data from web*/
-	static string GetDataFromWeb()
-	{
-		WebClient client = new WebClient();
-		string flagData = client.DownloadString("https://archive.ics.uci.edu/ml/machine-learning-databases/flags/flag.data");
-		return flagData;
+		static Vector<double> Normalization(Vector<double> arr)
+		{
+			Vector<double> arrNormalized = Vector<double>.Build.Dense(arr.Count);
+			double min = arr.Min();
+			double max = arr.Max();
+			for (int i = 0; i < arr.Count; i++)
+				arrNormalized[i] = (arr[i] - min) / (max - min);
+			return arrNormalized;
+		}
+
+		/* 
+		 * Get data matrix:
+		 * replace country name to index;
+		 * replace color to color number
+		*/
+		private static Matrix<double> ConvertToDataMatrix(string flagData)
+		{
+			int countryCounter = 0;
+			List<string> arrLines = flagData.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+			Matrix<double> dataMatrix = Matrix<double>.Build.Dense(arrLines.Count, 30); /*matrix 194x30*/
+			foreach (string line in arrLines)
+			{
+				string newLine = Regex.Replace(line, string.Join("|", ColorReplacements.Keys.Select(k => k.ToString()).ToArray()), m => ColorReplacements[m.Value]); /*replace color to number*/
+				string[] arr = newLine.Split(',').ToArray();
+				Country.Add(arr[0], countryCounter); /*add country to dictionary: key=country, value=number*/
+				arr[0] = countryCounter.ToString(); /*replace country to number*/
+				if (int.Parse(arr[22]) > 0) /*if flag have >0 sunstars, then 1 (true), if ==0, then 0 (false) */
+				{
+					arr[22] = "1";
+				}
+				else
+				{
+					arr[22] = "0";
+				}
+				dataMatrix.SetRow(countryCounter, Array.ConvertAll(arr, double.Parse)); /*parse from string to to int array and add to matrix row*/
+				countryCounter++;
+			}
+			return dataMatrix;
+		}
+
+		/*Get flag data from web*/
+		static string GetDataFromWeb()
+		{
+			WebClient client = new WebClient();
+			string flagData = client.DownloadString("https://archive.ics.uci.edu/ml/machine-learning-databases/flags/flag.data");
+			return flagData;
+		}
 	}
-}
 }
